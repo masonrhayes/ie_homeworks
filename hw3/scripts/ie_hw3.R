3b01141b29b68a478561fbcaf11a037456bc2902
vanlik <vanlik.tan@gmail.com>
2020-11-11 15:09:13 +0100

    q5~q8 fix

--- hw3/scripts/ie_hw3.R
+++ hw3/scripts/ie_hw3.R
@@ -32,3 +32,25 @@ summary(q4_model)
 # The following fixes it but it is not ideal..
 na.omit(summary(q4_model)$coef)[,1:2]
 
+
+### Question 6 ------------
+
+#using ivreg
+
+q6_model = ivreg(LWKLYWGE ~ EDUC + MARRIED + REGION + YOB | YOB_int_QOB + QOB, data = census)
+summary(q6_model)
+
+### Question 8
+
+# Hausman test ----
+betaOLS_educ <- coefficients(q1_model)[2]
+varOLS_educ <- vcov(q1_model)[2,2]
+betaIV_educ <- coefficients(q6_model)[2]
+varIV_educ <- vcov(q6_model)[2,2]
+
+hausman <- (betaOLS_educ - betaIV_educ) / sqrt(varIV_educ - varOLS_educ)
+hausman
+
+# |t| = 0.7: We can't reject H0  
+
+
